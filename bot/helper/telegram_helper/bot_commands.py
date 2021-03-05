bb287fb43afbbd708a25e5e744a738059c04b80c
downsekolah <76230202+downsekolah@users.noreply.github.com>
2021-03-06 01:04:09 +0800

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,10 +1,10 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'mulai'
-        self.MirrorCommand = 'kaca'
-        self.UnzipMirrorCommand = 'bukaarsip'
+        self.MirrorCommand = 'gas'
+        self.UnzipMirrorCommand = 'bukazip'
         self.TarMirrorCommand = 'tarkaca'
-        self.CancelMirror = 'gagal'
+        self.CancelMirror = 'batal'
         self.CancelAllCommand = 'batalin'
         self.ListCommand = 'daftar'
         self.SpeedCommand = 'testkecepatan'
@@ -14,7 +14,7 @@ class _BotCommands:
         self.PingCommand = 'ping'
         self.RestartCommand = 'restart'
         self.StatsCommand = 'stats'
-        self.HelpCommand = 'tolong'
+        self.HelpCommand = 'dancok'
         self.LogCommand = 'log'
         self.CloneCommand = "cln"
         self.WatchCommand = 'nonton'
